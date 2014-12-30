c4e8a05726635e2b967ff15fce51dbe07be6d828
pbielicki <przemyslaw.bielicki@amadeus.com>
2014-12-30 08:38:02 +0100

    Added warning in logs in case environment property is not set

--- src/main/java/com/bielu/oshift/Utils.java
+++ src/main/java/com/bielu/oshift/Utils.java
@@ -1,11 +1,18 @@
 package com.bielu.oshift;
 
+import java.util.logging.Logger;
+
 public class Utils {
+  
+  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
   public static String getenv(String variable, String... defaultValue) {
     String result = System.getenv(variable);
-    if (result == null && defaultValue.length == 1) {
-      return defaultValue[0];
+    if (result == null) {
+      LOG.warning("Environment variable '" + variable + "' not set. This might cause system instability.");
+      if (defaultValue.length == 1) {
+        return defaultValue[0];
+      }
     }
     
     return result;
