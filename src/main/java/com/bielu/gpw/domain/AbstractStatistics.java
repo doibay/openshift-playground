cbc20fca3c5a87109a1ea9afb23590bd487fd150
pbielicki <przemyslaw.bielicki@amadeus.com>
2015-09-04 08:42:17 +0200

    Added NPE check

--- src/main/java/com/bielu/gpw/domain/AbstractStatistics.java
+++ src/main/java/com/bielu/gpw/domain/AbstractStatistics.java
@@ -8,16 +8,25 @@ import com.bielu.gpw.Util;
 
 public abstract class AbstractStatistics {
 
-  @XmlAttribute(name = "iv") String initValue;
-  @XmlAttribute(name = "vnet") String valueNet;
-  @XmlAttribute(name = "pnet") String profitNet;
-  @XmlAttribute(name = "ptaxed") String profitNetTaxed;
-  @XmlAttribute(name = "r") String rate;
-  
+  @XmlAttribute(name = "iv")
+  String initValue;
+  @XmlAttribute(name = "vnet")
+  String valueNet;
+  @XmlAttribute(name = "pnet")
+  String profitNet;
+  @XmlAttribute(name = "ptaxed")
+  String profitNetTaxed;
+  @XmlAttribute(name = "r")
+  String rate;
+
   public AbstractStatistics() {
   }
-  
+
   protected AbstractStatistics(Investment initial, Investment current) {
+    if (initial == null || current == null) {
+      return;
+    }
+
     initValue = format(initial.value());
     valueNet = format(current.netValue());
     BigDecimal profitNet = current.netValue().subtract(initial.netValue());
@@ -27,10 +36,10 @@ public abstract class AbstractStatistics {
     } else {
       profitNetTaxed = format(profitNet);
     }
-    
+
     rate = format(profitNet.divide(current.netValue()).multiply(BigDecimal.valueOf(100))) + "%";
   }
-  
+
   protected String format(BigDecimal value) {
     return String.format(Util.FORMAT_MONEY, value);
   }
